6cee317f68356f78ca2288ff270a8df8f71ae0e1
telnarr <saraydurdy@gmail.com>
2025-10-05 15:06:42 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,8 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 # Gemini ayarı
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel("models/gemini-2.0-flash")
+
 
 # Bot
 bot = Bot(token=TELEGRAM_TOKEN)
